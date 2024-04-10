d9a3a3e799f0e79344f85b5394a5020d61d9296f
LeeJaeHong <0915jhlee@daum.net>
2024-04-10 22:32:35 +0900

    Add: 메인 컨트롤러
    홈, 더하기 페이지 이동

--- src/main/java/Side/Gallery/webcontroller/GalleryController.java
+++ src/main/java/Side/Gallery/webcontroller/GalleryController.java
@@ -1,20 +1,45 @@
 package Side.Gallery.webcontroller;
 
 
+import Side.Gallery.domain.Picture;
+import Side.Gallery.service.GalleryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/gallery")
+@Slf4j
 public class GalleryController {
 
+    private final GalleryService galleryService;
     @GetMapping
     public String gallery(){
 
         return "home";
+    }
+
+    @GetMapping("/add")
+    public String addPage(){
+        return "addPicture";
+    }
+
+
+
+    @PostMapping("/add")
+    public String addPicture(@ModelAttribute Picture picture, Model model){
+        Picture inputPicture = galleryService.save(picture);
+        Picture a = galleryService.findById(inputPicture.getId()).get();
+
+        log.info("src = {}", a.getData());
+        model.addAttribute("picture", a);
+        return "redirect:/";
     }
 
 }
