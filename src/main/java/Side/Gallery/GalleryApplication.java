eeaf1bb845fba8f7f2709d4347531e6c4c7a6105
LeeJaeHong <0915jhlee@daum.net>
2024-04-13 14:23:34 +0900

    Feat: scanBasePackages 지정

--- src/main/java/Side/Gallery/GalleryApplication.java
+++ src/main/java/Side/Gallery/GalleryApplication.java
@@ -1,9 +1,12 @@
 package Side.Gallery;
 
+import Side.Gallery.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(AppConfig.class)
+@SpringBootApplication(scanBasePackages = "Side.Gallery.webcontroller")
 public class GalleryApplication {
 
 	public static void main(String[] args) {
