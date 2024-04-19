2dc7b73573ea4cf37f999aa40e62be670e13e180
LeeJaeHong <0915jhlee@daum.net>
2024-04-19 22:29:05 +0900

    feat: save 기능에 트랜잭션 적용
    저장 단계에서 오류 발생시 롤백되게하기 위함

--- src/main/java/Side/Gallery/service/MainServiceImpl.java
+++ src/main/java/Side/Gallery/service/MainServiceImpl.java
@@ -3,6 +3,7 @@ package Side.Gallery.service;
 import Side.Gallery.domain.Picture;
 import Side.Gallery.repository.TotalRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +16,7 @@ public class MainServiceImpl implements GalleryService{
         this.pictureRepository = pictureRepository;
     }
 
+    @Transactional
     @Override
     public Picture save(Picture picture) {
         return pictureRepository.save(picture);
